1ee8fdc1c772fcc843aae835c07b805f5be46fcf
Ashutosh Mishrara <ashutosh.mishra41@gmail.com>
2020-04-24 04:15:42 +0530

    LeetCode Question: 392. Is Subsequence

---
+++ Solutions/Dynamic_Programming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using isSubsequence;
+
+namespace Dynamic_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new isSubsequence.Solution();
+            var result = solution.IsSubsequence( new string("suh"), new string("Asthoou"));
+            Console.WriteLine(result);
+
+        }
+    }
+}
